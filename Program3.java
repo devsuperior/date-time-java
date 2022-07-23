2a19f57d873c5c57f6dc10b146eefb3824c8a138
Nelio Alves <acenelio@gmail.com>
2022-07-23 19:49:39 -0300

    Update Program3.java

--- Program3.java
+++ Program3.java
@@ -9,25 +9,29 @@ public class Program3 {
 
 	public static void main(String[] args) {
 
-		//for (String s : ZoneId.getAvailableZoneIds())
-		//	System.out.println(s);
+		// for (String s : ZoneId.getAvailableZoneIds())
+		// System.out.println(s);
 
 		LocalDate d04 = LocalDate.parse("2022-07-20");
 		LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
 		Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
 
-		System.out.println("d04 dia = " + d04.getDayOfMonth());
-		System.out.println("d04 mês = " + d04.getMonthValue());
-		System.out.println("d04 ano = " + d04.getYear());
-		System.out.println("d05 hora = " + d05.getHour());
-		System.out.println("d05 minutos = " + d05.getMinute());
-
 		LocalDate r1 = LocalDate.ofInstant(d06, ZoneId.systemDefault());
-		LocalDateTime r2 = LocalDateTime.ofInstant(d06, ZoneId.systemDefault());
-		LocalDateTime r3 = LocalDateTime.ofInstant(d06, ZoneId.of("Portugal"));
+		LocalDate r2 = LocalDate.ofInstant(d06, ZoneId.of("Portugal"));
+		LocalDateTime r3 = LocalDateTime.ofInstant(d06, ZoneId.systemDefault());
+		LocalDateTime r4 = LocalDateTime.ofInstant(d06, ZoneId.of("Portugal"));
 
 		System.out.println("r1 = " + r1);
 		System.out.println("r2 = " + r2);
 		System.out.println("r3 = " + r3);
+		System.out.println("r4 = " + r4);
+
+		System.out.println("d04 dia = " + d04.getDayOfMonth());
+		System.out.println("d04 mês = " + d04.getMonthValue());
+		System.out.println("d04 ano = " + d04.getYear());
+
+		System.out.println("d05 hora = " + d05.getHour());
+		System.out.println("d05 hora = " + d05.getMinute());
+
 	}
 }
